6d311a9ab8d311aa71e89b8cb7854d470ec1f434
Anuj Gupta <junagupta19@gmail.com>
2017-12-03 02:42:49 +0530

    upload functional

--- Upload.php
+++ Upload.php
@@ -2,6 +2,23 @@
 
 
 
+   // $file = file_get_contents($_POST['file1']['path'], true);
+   //echo $file;
+  //echo sha1_file('/Applications/XAMPP/xamppfiles/htdocs/Hashcoin/Upload.php');
+  //echo sha1_file($_FILES['file1']['name']);
+
+  $targetdir = '/Applications/XAMPP/xamppfiles/htdocs/Hashcoin/uploads/';
+      // name of the directory where the files should be stored
+  $targetfile = $targetdir.$_FILES['file1']['name'];
+
+  if (move_uploaded_file($_FILES['file1']['tmp_name'], $targetfile)) {
+    // file uploaded succeeded
+    echo "done";
+    echo sha1_file($targetfile);
+  } else {
+    // file upload failed
+  }
+
  ?>
 
 <!DOCTYPE html>
@@ -22,8 +39,8 @@
   </script>
 
   <body>
-    <form action="Upload.php" method="post">
-    <input id="file_upload" type="file">
+    <form action="Upload.php" method="post" enctype="multipart/form-data">
+    <input id="file_upload" type="file" name="file1">
     <button id="submit">Submit</button>
   </form>
   </body>
